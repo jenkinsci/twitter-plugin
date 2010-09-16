fabe47ed8fe7686433244b6c56705f2c20f67e91
cactusman <cactusman@dev.java.net>
2010-09-16 13:38:22 +0000

    [maven-scm] copy for tag twitter-0.5

---
+++ twitter/auth/src/main/java/org/jvnet/hudson/plugins/twitter/Main.java
@@ -0,0 +1,49 @@
+package org.jvnet.hudson.plugins.twitter;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.http.AccessToken;
+import twitter4j.http.RequestToken;
+
+public class Main {
+	private static final String CONSUMER_KEY = "8B6nAb0a5QScWxROd5oWA";;
+	private static final String CONSUMER_SECRET = "pXO0lgCZYUvix7Ay7YLdsIep38VBiH2cTldOeMj1J5s";
+	
+	public static void main(String[] args) throws Exception {
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+		RequestToken requestToken = twitter.getOAuthRequestToken();
+		AccessToken accessToken = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (null == accessToken) {
+			System.out.println("Open the following URL and grant access to your account:");
+			System.out.println(requestToken.getAuthorizationURL());
+			System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+			String pin = br.readLine();
+			try{
+				if(pin.length() > 0){
+					accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+				}else{
+					accessToken = twitter.getOAuthAccessToken();
+				}
+			} catch (TwitterException te) {
+				if(401 == te.getStatusCode()){
+					System.out.println("Unable to get the access token.");
+				}else{
+					te.printStackTrace();
+				}
+				System.exit(1);
+			}
+		}
+		storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
+		System.exit(0);
+	}
+	private static void storeAccessToken(int useId, AccessToken accessToken){
+		System.out.println("access token:" + accessToken.getToken());
+		System.out.println("access token secret:" + accessToken.getTokenSecret());
+	}
+}
---
+++ twitter/src/main/java/hudson/plugins/twitter/UserTwitterProperty.java
@@ -0,0 +1,76 @@
+package hudson.plugins.twitter;
+
+import hudson.model.User;
+import hudson.model.UserProperty;
+import hudson.model.UserPropertyDescriptor;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
+
+/**
+ * UserProperty class which contains a user's twitter id.
+ * 
+ * @author landir
+ */
+@ExportedBean(defaultVisibility = 999)
+public class UserTwitterProperty extends UserProperty {
+
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+
+    private String twitterid;
+
+    public UserTwitterProperty() {
+    }
+
+    @DataBoundConstructor
+    public UserTwitterProperty(String twitterid) {
+        this.twitterid = twitterid;
+    }
+
+    public UserPropertyDescriptor getDescriptor() {
+        return DESCRIPTOR;
+    }
+
+    @Exported
+    public User getUser() {
+        return user;
+    }
+
+    @Exported
+    public String getTwitterid() {
+        return twitterid;
+    }
+
+    public void setTwitterid(String twitterid) {
+        this.twitterid = twitterid;
+    }
+
+    public static final class DescriptorImpl extends UserPropertyDescriptor {
+        public DescriptorImpl() {
+            super(UserTwitterProperty.class);
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Twitter User Name";
+        }
+
+        @Override
+        public UserTwitterProperty newInstance(StaplerRequest req, JSONObject formData)
+                throws hudson.model.Descriptor.FormException {
+            if (formData.has("twitterid")) {
+                return req.bindJSON(UserTwitterProperty.class, formData);
+            } else {
+                return new UserTwitterProperty();
+            }
+        }
+
+        @Override
+        public UserProperty newInstance(User user) {
+            return null;
+        }
+    }
+}
