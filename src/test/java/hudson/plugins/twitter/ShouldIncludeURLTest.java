e905eb50f93586958548cbae280e9ac51920b28e
cactusman <cactusman@dev.java.net>
2010-09-20 05:36:12 +0000

    Fixed test case.

--- src/test/java/hudson/plugins/twitter/ShouldIncludeURLTest.java
+++ src/test/java/hudson/plugins/twitter/ShouldIncludeURLTest.java
@@ -8,32 +8,32 @@ import org.jvnet.hudson.test.HudsonTestCase;
 public class ShouldIncludeURLTest extends HudsonTestCase {
 
     public void testTrueInDescriptor() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.includeUrl = true;
-//
-//        TwitterPublisher pub = new TwitterPublisher(null, null);
-//        Assert.assertTrue(pub.shouldIncludeUrl());
-//
-//        ReflectionHelper.setField(pub, "includeUrl", Boolean.FALSE);
-//        Assert.assertFalse(pub.shouldIncludeUrl());
-//
-//        ReflectionHelper.setField(pub, "includeUrl", Boolean.TRUE);
-//        Assert.assertTrue(pub.shouldIncludeUrl());
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.includeUrl = true;
+
+        TwitterPublisher pub = new TwitterPublisher(null, null);
+        Assert.assertTrue(pub.shouldIncludeUrl());
+
+        ReflectionHelper.setField(pub, "includeUrl", Boolean.FALSE);
+        Assert.assertFalse(pub.shouldIncludeUrl());
+
+        ReflectionHelper.setField(pub, "includeUrl", Boolean.TRUE);
+        Assert.assertTrue(pub.shouldIncludeUrl());
     }
 
-//    public void testFalseInDescriptor() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.includeUrl = false;
-//
-//        TwitterPublisher pub = new TwitterPublisher(null, null);
-//
-//        Assert.assertFalse(pub.shouldIncludeUrl());
-//
-//        ReflectionHelper.setField(pub, "includeUrl", Boolean.FALSE);
-//        Assert.assertFalse(pub.shouldIncludeUrl());
-//
-//        ReflectionHelper.setField(pub, "includeUrl", Boolean.TRUE);
-//        Assert.assertTrue(pub.shouldIncludeUrl());
-//    }
+    public void testFalseInDescriptor() throws Exception {
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.includeUrl = false;
+
+        TwitterPublisher pub = new TwitterPublisher(null, null);
+
+        Assert.assertFalse(pub.shouldIncludeUrl());
+
+        ReflectionHelper.setField(pub, "includeUrl", Boolean.FALSE);
+        Assert.assertFalse(pub.shouldIncludeUrl());
+
+        ReflectionHelper.setField(pub, "includeUrl", Boolean.TRUE);
+        Assert.assertTrue(pub.shouldIncludeUrl());
+    }
 
 }
--- src/test/java/hudson/plugins/twitter/ShouldTweetTest.java
+++ src/test/java/hudson/plugins/twitter/ShouldTweetTest.java
@@ -17,150 +17,150 @@ import org.jvnet.hudson.test.HudsonTestCase;
 public class ShouldTweetTest extends HudsonTestCase {
 
     public void testWhenDescriptorTrueAndInstanceNull() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.onlyOnFailureOrRecovery = true;
-//
-//        TwitterPublisher pub = new TwitterPublisher(null, null);
-//
-//        assertLimitedTweets(pub);
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.onlyOnFailureOrRecovery = true;
+
+        TwitterPublisher pub = new TwitterPublisher(null, null);
+
+        assertLimitedTweets(pub);
     }
-//
-//    public void testWhenDescriptorTrueAndInstanceFalse() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.onlyOnFailureOrRecovery = true;
-//
-//        TwitterPublisher pub = new TwitterPublisher("false", null);
-//
-//        assertAlwaysTweet(pub);
-//    }
-//
-//    public void testWhenDescriptorTrueAndInstanceTrue() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.onlyOnFailureOrRecovery = true;
-//
-//        TwitterPublisher pub = new TwitterPublisher("true", null);
-//
-//        assertLimitedTweets(pub);
-//    }
-//
-//    public void testWhenDescriptorFalseAndInstanceNull() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.onlyOnFailureOrRecovery = false;
-//
-//        TwitterPublisher pub = new TwitterPublisher(null, null);
-//
-//        assertAlwaysTweet(pub);
-//    }
-//
-//    public void testWhenDescriptorFalseAndInstanceFalse() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.onlyOnFailureOrRecovery = false;
-//
-//        TwitterPublisher pub = new TwitterPublisher("false", null);
-//
-//        assertAlwaysTweet(pub);
-//    }
-//
-//    public void testWhenDescriptorFalseAndInstanceTrue() throws Exception {
-//        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
-//        descriptor.onlyOnFailureOrRecovery = false;
-//
-//        TwitterPublisher pub = new TwitterPublisher("true", null);
-//
-//        assertLimitedTweets(pub);
-//    }
-//
-//    private void assertAlwaysTweet(TwitterPublisher pub) throws Exception {
-//        FreeStyleBuild successfulBuildAfterNothing = createSuccessfulBuild();
-//        assertTrue(pub.shouldTweet(successfulBuildAfterNothing));
-//
-//        FreeStyleBuild successfulBuildAfterSuccess = createSuccessfulBuildAfterSuccess();
-//        assertTrue(pub.shouldTweet(successfulBuildAfterSuccess));
-//
-//        FreeStyleBuild successfulBuildAfterFailure = createSuccessfulBuildAfterFailure();
-//        assertTrue(pub.shouldTweet(successfulBuildAfterFailure));
-//
-//        FreeStyleBuild failedBuild = createFailedBuild();
-//        assertTrue(pub.shouldTweet(failedBuild));
-//
-//        FreeStyleBuild unstableBuild = createUnstableBuild();
-//        assertTrue(pub.shouldTweet(unstableBuild));
-//
-//        FreeStyleBuild abortedBuild = createAbortedBuild();
-//        assertTrue(pub.shouldTweet(abortedBuild));
-//    }
-//
-//    private FreeStyleBuild createSuccessfulBuild() throws Exception {
-//        FreeStyleProject prj = createFreeStyleProject();
-//        return prj.scheduleBuild2(0).get();
-//    }
-//
-//    private FreeStyleBuild createSuccessfulBuildAfterFailure() throws Exception {
-//        FreeStyleProject prj = createFreeStyleProject();
-//        prj.getBuildersList().add(new FailureBuilder());
-//        prj.scheduleBuild2(0).get();
-//        prj.getBuildersList().clear();
-//        return prj.scheduleBuild2(0).get();
-//    }
-//
-//    private FreeStyleBuild createSuccessfulBuildAfterSuccess() throws Exception {
-//        FreeStyleProject prj = createFreeStyleProject();
-//        prj.scheduleBuild2(0).get();
-//        return prj.scheduleBuild2(0).get();
-//    }
-//
-//    private void assertLimitedTweets(TwitterPublisher pub) throws Exception {
-//        FreeStyleBuild successfulBuildAfterNothing = createSuccessfulBuild();
-//        assertFalse(pub.shouldTweet(successfulBuildAfterNothing));
-//
-//        FreeStyleBuild successfulBuildAfterSuccess = createSuccessfulBuildAfterSuccess();
-//        assertFalse(pub.shouldTweet(successfulBuildAfterSuccess));
-//
-//        FreeStyleBuild successfulBuildAfterFailure = createSuccessfulBuildAfterFailure();
-//        assertTrue(pub.shouldTweet(successfulBuildAfterFailure));
-//
-//        FreeStyleBuild failedBuild = createFailedBuild();
-//        assertTrue(pub.shouldTweet(failedBuild));
-//
-//        FreeStyleBuild unstableBuild = createUnstableBuild();
-//        assertTrue(pub.shouldTweet(unstableBuild));
-//
-//        FreeStyleBuild abortedBuild = createAbortedBuild();
-//        assertFalse(pub.shouldTweet(abortedBuild));
-//    }
-//
-//    private FreeStyleBuild createFailedBuild() throws Exception {
-//        return createBuild(new FailureBuilder());
-//    }
-//
-//    private FreeStyleBuild createBuild(Builder b) throws Exception {
-//        FreeStyleProject prj = createFreeStyleProject();
-//        prj.getBuildersList().add(b);
-//        return prj.scheduleBuild2(0).get();
-//    }
-//
-//    private FreeStyleBuild createUnstableBuild() throws Exception {
-//        return createBuild(new UnstableBuilder());
-//    }
-//
-//    private FreeStyleBuild createAbortedBuild() throws Exception {
-//        return createBuild(new AbortedBuilder());
-//    }
-//
-//    public static class UnstableBuilder extends Builder {
-//        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
-//                throws InterruptedException, IOException {
-//            build.setResult(Result.UNSTABLE);
-//            return false;
-//        }
-//    }
-//
-//    public static class AbortedBuilder extends Builder {
-//        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
-//                throws InterruptedException, IOException {
-//            build.setResult(Result.ABORTED);
-//            return false;
-//        }
-//    }
+
+    public void testWhenDescriptorTrueAndInstanceFalse() throws Exception {
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.onlyOnFailureOrRecovery = true;
+
+        TwitterPublisher pub = new TwitterPublisher("false", null);
+
+        assertAlwaysTweet(pub);
+    }
+
+    public void testWhenDescriptorTrueAndInstanceTrue() throws Exception {
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.onlyOnFailureOrRecovery = true;
+
+        TwitterPublisher pub = new TwitterPublisher("true", null);
+
+        assertLimitedTweets(pub);
+    }
+
+    public void testWhenDescriptorFalseAndInstanceNull() throws Exception {
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.onlyOnFailureOrRecovery = false;
+
+        TwitterPublisher pub = new TwitterPublisher(null, null);
+
+        assertAlwaysTweet(pub);
+    }
+
+    public void testWhenDescriptorFalseAndInstanceFalse() throws Exception {
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.onlyOnFailureOrRecovery = false;
+
+        TwitterPublisher pub = new TwitterPublisher("false", null);
+
+        assertAlwaysTweet(pub);
+    }
+
+    public void testWhenDescriptorFalseAndInstanceTrue() throws Exception {
+        DescriptorImpl descriptor = hudson.getDescriptorByType(DescriptorImpl.class);
+        descriptor.onlyOnFailureOrRecovery = false;
+
+        TwitterPublisher pub = new TwitterPublisher("true", null);
+
+        assertLimitedTweets(pub);
+    }
+
+    private void assertAlwaysTweet(TwitterPublisher pub) throws Exception {
+        FreeStyleBuild successfulBuildAfterNothing = createSuccessfulBuild();
+        assertTrue(pub.shouldTweet(successfulBuildAfterNothing));
+
+        FreeStyleBuild successfulBuildAfterSuccess = createSuccessfulBuildAfterSuccess();
+        assertTrue(pub.shouldTweet(successfulBuildAfterSuccess));
+
+        FreeStyleBuild successfulBuildAfterFailure = createSuccessfulBuildAfterFailure();
+        assertTrue(pub.shouldTweet(successfulBuildAfterFailure));
+
+        FreeStyleBuild failedBuild = createFailedBuild();
+        assertTrue(pub.shouldTweet(failedBuild));
+
+        FreeStyleBuild unstableBuild = createUnstableBuild();
+        assertTrue(pub.shouldTweet(unstableBuild));
+
+        FreeStyleBuild abortedBuild = createAbortedBuild();
+        assertTrue(pub.shouldTweet(abortedBuild));
+    }
+
+    private FreeStyleBuild createSuccessfulBuild() throws Exception {
+        FreeStyleProject prj = createFreeStyleProject();
+        return prj.scheduleBuild2(0).get();
+    }
+
+    private FreeStyleBuild createSuccessfulBuildAfterFailure() throws Exception {
+        FreeStyleProject prj = createFreeStyleProject();
+        prj.getBuildersList().add(new FailureBuilder());
+        prj.scheduleBuild2(0).get();
+        prj.getBuildersList().clear();
+        return prj.scheduleBuild2(0).get();
+    }
+
+    private FreeStyleBuild createSuccessfulBuildAfterSuccess() throws Exception {
+        FreeStyleProject prj = createFreeStyleProject();
+        prj.scheduleBuild2(0).get();
+        return prj.scheduleBuild2(0).get();
+    }
+
+    private void assertLimitedTweets(TwitterPublisher pub) throws Exception {
+        FreeStyleBuild successfulBuildAfterNothing = createSuccessfulBuild();
+        assertFalse(pub.shouldTweet(successfulBuildAfterNothing));
+
+        FreeStyleBuild successfulBuildAfterSuccess = createSuccessfulBuildAfterSuccess();
+        assertFalse(pub.shouldTweet(successfulBuildAfterSuccess));
+
+        FreeStyleBuild successfulBuildAfterFailure = createSuccessfulBuildAfterFailure();
+        assertTrue(pub.shouldTweet(successfulBuildAfterFailure));
+
+        FreeStyleBuild failedBuild = createFailedBuild();
+        assertTrue(pub.shouldTweet(failedBuild));
+
+        FreeStyleBuild unstableBuild = createUnstableBuild();
+        assertTrue(pub.shouldTweet(unstableBuild));
+
+        FreeStyleBuild abortedBuild = createAbortedBuild();
+        assertFalse(pub.shouldTweet(abortedBuild));
+    }
+
+    private FreeStyleBuild createFailedBuild() throws Exception {
+        return createBuild(new FailureBuilder());
+    }
+
+    private FreeStyleBuild createBuild(Builder b) throws Exception {
+        FreeStyleProject prj = createFreeStyleProject();
+        prj.getBuildersList().add(b);
+        return prj.scheduleBuild2(0).get();
+    }
+
+    private FreeStyleBuild createUnstableBuild() throws Exception {
+        return createBuild(new UnstableBuilder());
+    }
+
+    private FreeStyleBuild createAbortedBuild() throws Exception {
+        return createBuild(new AbortedBuilder());
+    }
+
+    public static class UnstableBuilder extends Builder {
+        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+                throws InterruptedException, IOException {
+            build.setResult(Result.UNSTABLE);
+            return false;
+        }
+    }
+
+    public static class AbortedBuilder extends Builder {
+        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+                throws InterruptedException, IOException {
+            build.setResult(Result.ABORTED);
+            return false;
+        }
+    }
 
 }
